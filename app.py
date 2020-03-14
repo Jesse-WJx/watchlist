301339473c11d675b420dae5c8145d9bf3acecbc
Jesse-WJx <1025693798@qq.com>
2020-03-14 07:22:08 -0700

    Create.edit and delete item by form

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import click
 
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask import flash
 
 prefix = 'sqlite:////'
 
@@ -59,10 +61,25 @@ def forge():
 
 
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST']) #在app.route()装饰器里，可以用methods关键字传递一个包含HTTP方法字符串的列表，
+def index():                            #表示这个视图函数处理哪种方法类型的请求
+    if request.method == 'POST':    #判断是否是post请求
+        #获取表单数据,request.form是一个特殊字典，用表单字段的name属性可以获取用户填入的对应数据
+        title = request.form.get('title')   #传入表单对应输入字段的name值
+        year = request.form.get('year')
+        #验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.') #显示错误提示
+            return redirect(url_for('index'))      #重定向回主页
+        #保存表单数据到数据库
+        movie = Movie(title=title, year=year)   #创建记录
+        db.session.add(movie)   #添加到数据库会话
+        db.session.commit   #提交数据库会话
+        flash('Item created.')  #显示创建成功的提示,flash()函数用来在是凸函数里向模板传递提示消息
+        return redirected(url_for('index'))
+    user = User.query.first()
     movies = Movie.query.all()  #读取所有的电影记录
-    return render_template('index.html',movies=movies)
+    return render_template('index.html',user=user,movies=movies)
 
 @app.cli.command()  #注册为命令
 #使用click提供的option装饰器为命令添加一个 --drop选项，将is_flag参数设置为True,就可以将这个选项声明为布尔值标志，
@@ -81,6 +98,36 @@ def initdb(drop):
 def page_not_found(e):  #接受异常对象作为参数
     return render_template('404.html'),404    #返回模板和状态码
 
+#编辑电影条目
+@app.route('/movie/edit/<int:movie_id>', methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':    #处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) >60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  #重定向回对应的编辑页面
+
+        movie.title = title #更新标题
+        movie.year = year   #更新年份
+        db.session.commit() #提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))   #重定向回主页
+
+    return render_template('edit.html',movie=movie) #c传入被编辑的电影记录
+app.config['SECRET_KEY'] = 'dev'
+
+#删除电影条目
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])    #限定只接受post请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)    #获取电影记录
+    db.session.delete(movie)    #删除对应的记录
+    db.session.commit() #提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))
 
 
 
